47048fd5bdc99308ecaa1b9847556e350d7ab18c
ssigg <stephan.sigg@gmail.com>
2016-10-02 19:40:40 +0200

    Group admin routes

--- admin/index.php
+++ admin/index.php
@@ -10,41 +10,30 @@ require '../dependencies.php';
 // Routes
 // =============================================================
 $app->get('/events', Actions\ListEventsAction::class);
-
 $app->get('/events/{id}', Actions\GetEventAction::class);
-
 $app->post('/events', Actions\CreateEventAction::class);
-
-$app->put('/events/{id}', Actions\CreateEventAction::class);
+$app->put('/events/{id}', Actions\ChangeEventAction::class);
+$app->delete('/events/{id}', Actions\DeleteEventAction::class);
 
 $app->get('/eventblocks', Actions\ListEventblocksAction::class);
-
 $app->get('/eventblocks/{id}', Actions\GetEventblockAction::class);
-
 $app->put('/eventblocks/{id}', Actions\ChangeEventblockAction::class);
-
 $app->delete('/eventblocks/{id}', Actions\DeleteEventblockAction::class);
 
 $app->get('/blocks', Actions\ListBlocksAction::class);
-
 $app->get('/blocks/{id}', Actions\GetBlockAction::class);
-
 $app->post('/blocks', Actions\CreateBlockAction::class);
-
 $app->put('/blocks/{id}', Actions\ChangeBlockAction::class);
+$app->delete('/blocks/{id}', Actions\DeleteBlockAction::class);
 
 $app->get('/categories', Actions\ListCategoriesAction::class);
-
 $app->post('/categories', Actions\CreateCategoryAction::class);
-
 $app->put('/categories/{id}', Actions\ChangeCategoryAction::class);
-
 $app->delete('/categories/{id}', Actions\DeleteCategoryAction::class);
 
 $app->get('/reservations', Actions\ListReservationsAction::class);
 
 $app->post('/seats', Actions\CreateSeatsAction::class);
-
 $app->delete('/seats/{id}', Actions\DeleteSeatAction::class);
 // =============================================================
 
