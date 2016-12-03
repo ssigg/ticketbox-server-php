84990edbecbcf304f459705716304996ecba45ac
ssigg <stephan.sigg@gmail.com>
2016-12-03 11:32:01 +0100

    Group actions

--- boxoffice/index.php
+++ boxoffice/index.php
@@ -10,17 +10,13 @@ require '../dependencies.php';
 // Routes
 // =============================================================
 $app->get('/events', Actions\ListEventsAction::class);
-
 $app->get('/events/{id}', Actions\GetEventAction::class);
 
 $app->get('/eventblocks/{id}', Actions\GetEventblockAction::class);
 
 $app->get('/reservations', Actions\ListMyReservationsAction::class);
-
 $app->post('/reservations', Actions\CreateReservationAction::class);
-
 $app->put('/reservations/{id}', Actions\ChangeReductionForReservationAction::class);
-
 $app->delete('/reservations/{id}', Actions\DeleteReservationAction::class);
 
 $app->post('/boxoffice-purchases', Actions\CreateBoxofficePurchaseAction::class);
--- customer/index.php
+++ customer/index.php
@@ -10,22 +10,16 @@ require '../dependencies.php';
 // Routes
 // =============================================================
 $app->get('/events', Actions\ListEventsAction::class);
-
 $app->get('/events/{id}', Actions\GetEventAction::class);
 
 $app->get('/eventblocks/{id}', Actions\GetEventblockAction::class);
 
 $app->get('/reservations', Actions\ListMyReservationsAction::class);
-
 $app->post('/reservations', Actions\CreateReservationAction::class);
-
 $app->put('/reservations/{id}', Actions\ChangeReductionForReservationAction::class);
-
 $app->delete('/reservations/{id}', Actions\DeleteReservationAction::class);
 
 $app->post('/orders', Actions\CreateOrderAction::class);
-
-$app->get('/print', Actions\PrintTicketAction::class);
 // =============================================================
 
 $app->run();
